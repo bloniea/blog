d6f5916657a61093823cbc4c17d5b6aa8409e8a3
bloniea <bloniea@qq.com>
2024-04-25 09:59:33 +0800

    显示版权

--- components/Copyright/index.vue
+++ components/Copyright/index.vue
@@ -3,15 +3,16 @@
 </template>
 
 <script setup>
-import { ref } from '@vue/reactivity'
+import { ref } from "@vue/reactivity"
 
 const nowYear = new Date().getFullYear()
 const year = ref(0)
-year.value = nowYear > 2022 ? 2022 + '-' + nowYear : 2022
+year.value = nowYear > 2022 ? 2022 + "-" + nowYear : 2022
 </script>
 
 <style lang="stylus" scoped>
 .copyright {
+  z-index 1
   background rgba(255, 255, 255, 0.6)
   padding 10px
   text-align center
